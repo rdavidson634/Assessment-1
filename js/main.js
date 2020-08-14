80aab3c2a0875c9629cd5481b202693175950d68
rdavidson634 <rdavidson634@gmail.com>
2020-08-14 15:32:23 -0600

    Final Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// app state
+let runningTotal = 0;
+
+// cached elements 
+const input = document.querySelector(".input");
+const total = document.querySelector(".totalCount");
+
+// event listeners
+document.querySelector(".subtractButton").addEventListener('click', handleSubClick);
+document.querySelector(".addButton").addEventListener('click', handleAddClick);
+
+// functions
+function init () {
+  total.textContent = 0;
+  input.value = 1;
+  render()
+}
+function render () {
+  total.textContent = runningTotal;
+}
+function handleSubClick () {
+  inputValue = parseInt(input.value, 10);
+    runningTotal -= inputValue;
+    inputValue = "";
+    render();
+}
+function handleAddClick () {
+  inputValue = parseInt(input.value, 10);
+    runningTotal += inputValue;
+    inputValue = "";
+    render()
+}
+
+
+init();
